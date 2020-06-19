86e5ea3cf12312dc7cac4f37cfef423034b2aa37
Kevin Koster <koster.k2001@gmail.com>
2020-06-19 12:16:37 +0200

    + rants can now be deleted at /rant/manage * updated the rant feed view + manage.html shows an overview of posts you have created with a delete button on them.

--- controllers/rant.py
+++ controllers/rant.py
@@ -20,6 +20,38 @@ def create():
     return dict(form=form)
 
 
+@auth.requires_login()
+def delete():
+    if not request.vars:
+        return '??'
+    rant_id = request.vars['rant_id']
+    rant = db.rant[rant_id]
+    if not rant:
+        raise NotImplementedError('Rant not found.')
+    if rant.author != auth.user_id:
+        session.flash = 'You do not have permission to do this.'
+        redirect(URL(f='feed'))
+    try:
+        db(db.rant.id == rant_id).delete()
+        db.commit()
+        session.flash = 'Rant has been deleted succesfully.'
+        redirect(URL(f='manage'))
+    except Exception:
+        raise
+
+
+@auth.requires_login()
+def manage():
+    query = db.rant.author == auth.user_id
+    orderby = ~db.rant.posted_at
+    pcache = (cache.ram, 15)
+    paginate = Pagination(db, query, orderby, display_count=10, cache=pcache, r=request, res=response)
+
+    rants = paginate.get_set(set_links=True)
+
+    return dict(rants=rants)
+
+
 def view():
     if request.vars['id']:
         rant_id = request.vars['id']
